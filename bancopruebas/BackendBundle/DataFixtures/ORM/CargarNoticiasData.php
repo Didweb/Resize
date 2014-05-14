ea7de1cbd42555b0cd2ca78f7d97cdf62b0e26fc
Eduard Pinuaga <info@did-web.com>
2014-05-14 09:40:54 +0200

    Fixtures para banco de pruebas

--- bancopruebas/BackendBundle/DataFixtures/ORM/CargarNoticiasData.php
+++ bancopruebas/BackendBundle/DataFixtures/ORM/CargarNoticiasData.php
@@ -1,21 +1,23 @@
 <?php
-namespace Acme\HelloBundle\DataFixtures\ORM;
+namespace bancopruebas\BackendBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
-use bancopruebas\NoticiaBundle\Entity\Noticia;
+use bancopruebas\BackendBundle\Entity\Noticia;
 
-class LoadUserData implements FixtureInterface
+class CargarNoticiasData implements FixtureInterface
 {
     /**
      * {@inheritDoc}
      */
     public function load(ObjectManager $manager)
     {
-       $faker = Faker\Factory::create(); 
+       $faker = \Faker\Factory::create(); 
        for($n=1;$n<=20;$n++){ 
-        $userAdmin = new User();
-        $userAdmin->setTitulo($faker->name);
+        $userAdmin = new Noticia();
+        $numcarac = rand(25,40);
+        $tit = substr($faker->text,0,$numcarac);
+        $userAdmin->setTitulo($tit);
         $userAdmin->setTexto($faker->text);
         $manager->persist($userAdmin);
 		}
